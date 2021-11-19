11f10e80606a47bf2b86cc5e52d954bb05007d20
Cihangir Ozmus <cozmus@ebay.com>
2021-11-19 10:49:10 +0300

    BubbleSort is updated.

--- src/sortingAlgorithms/bubbleSort/BubbleSort.java
+++ src/sortingAlgorithms/bubbleSort/BubbleSort.java
@@ -2,22 +2,25 @@ package sortingAlgorithms.bubbleSort;
 
 public class BubbleSort {
     public void sort(int[] array) {
-        boolean isSorted;
-        for (int i = 0; i < array.length; i++) {
-            isSorted = true;
+        if (array.length == 0) {
+            return;
+        }
 
-            for (int j = 1; j < array.length - i; j++)
-                if (array[j] < array[j - 1]) {
-                    swap(array, j, j - 1);
+        boolean isSorted = false;
+        int counter = 0;
+        while(!isSorted) {
+            isSorted = true;
+            for (int i = 0; i < array.length - 1 - counter; i++) {
+                if (array[i] > array[i + 1]) {
+                    swap(i, i + 1, array);
                     isSorted = false;
                 }
-
-            if (isSorted)
-                return;
+            }
+            counter++;
         }
     }
 
-    private void swap(int[] array, int index1, int index2) {
+    private void swap(int index1, int index2, int[] array) {
         var temp = array[index1];
         array[index1] = array[index2];
         array[index2] = temp;
